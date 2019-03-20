bac59d07bd91a476f4565ca05b1da6272cbdb4d9
Tori Snowden <tori@Toris-MacBook-Pro.local>
2019-03-20 17:53:58 -0600

    Commit 8 - I loops through their closets

--- JS/main.js
+++ JS/main.js
@@ -1,45 +1,45 @@
 //1. Easy Going
-//for ( var i = 1; i < 20; i++) {
-  //  console.log(i)
-//}
+for ( var i = 1; i < 20; i++) {
+  console.log(i)
+}
 
 //1. Get Even
-//for (var i = 0; i < 200; i+=2)
-//console.log(i)
+for (var i = 0; i < 200; i+=2)
+console.log(i)
 
 //1. Excited Kitten 
-//for (var i = 0; i < 20; i++)
-//console.log('Love me, pet me! HSSSSSS')
+for (var i = 0; i < 20; i++)
+console.log('Love me, pet me! HSSSSSS')
 
 //2. 
-/*const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
+const cat = ["...human...why you taking pictures of me?...", "...the catnip made me do it...", "...why does the red dot always get away..."]
 
 for (var i = 0; i < 20; i+=2) {
 var x = Math.floor(Math.random() * 3);
-console.log(cat[x]) */
-//}
+console.log(cat[x]) 
+}
  //1. Fizzbuzz
- //for (var i = 1; i < 100; i++) {
-//console.log(i) 
-//}
+ for (var i = 1; i <= 100; i++) {
+console.log(i) 
+}
 //2. 
-/*for (var i = 1; i < 100; i+=3) {
-    if ( i % 3 === "Fizz");
+for (var i = 1; i <= 100; i++) {
+    if ( i % 3 === 0 );
     console.log("Fizz") 
-}*/
+}
 //3.
-/*for (var i = 1; i < 100; i+=5) {
-    if ( i % 5 === "Buzz");
+for (var i = 1; i <= 100; i++) {
+    if ( i % 5 === 0 );
     console.log("Buzz") 
-} */
+} 
 //4. 
-/*for (var i = 1; i < 100; i++) {
-    if ( i % 3 && 1 % 5 === "FizzBuzz");
+for (var i = 1; i <= 100; i++) {
+    if ( i % 15 === 0 );
     console.log("FizzBuzz") 
-}*/
+}
 
 //1. Getting to Know you 
-/*const shahzad = ["Shahzad", 1000, "Austin"];
+const shahzad = ["Shahzad", 1000, "Austin"];
 const jim = ["Jim", 16, "All cities"];
 const reuben = ["Reuben", 22, "Durham"];
 const daniel = ["Daniel", 186, "Dallas"];
@@ -63,13 +63,13 @@ console.log(reuben[2])
 
 //5. 
 console.log(jim[2])
-*/
+
 
 //1. Yell at the Ninja Turtles 
-/*var ninjaTurtle = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
+var ninjaTurtle = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo']
 
 //2. 
-console.log(ninjaTurtle.map(a => a.toUpperCase()));*/
+console.log(ninjaTurtle.map(a => a.toUpperCase()));
 
 
 //1. Return of Closets 
@@ -105,11 +105,12 @@ const kristynsCloset = [
     ]
   ];
   //.1 Alien Attire
-  /*var kristynsShoe = kristynsCloset[0];
+  var kristynsShoe = kristynsCloset[0];
   thomsCloset[2].push(kristynsShoe);
   thomsCloset.shift();
-  console.log(thomsCloset) */
-//.1 Dress'em Up
+  console.log(thomsCloset) 
+
+  //.1 Dress'em Up
 
 kristynsCloset.forEach(function(element) {
     var x = Math.floor(Math.random() * 9);
@@ -120,3 +121,13 @@ thomsCloset.forEach(function(element) {
     var x = Math.floor(Math.random() * 9);
     console.log(thomsCloset[x])
 })
+//1. Dirty Laundry 
+for (var i = 0; i < kristynsCloset.length; i++) {
+    if (kristynsCloset[i] === true)
+    console.log("WHIRR Now washing ([i])")
+}
+//1. Inventory
+for (var i = 0; i < thomsCloset; i++) {
+let thomsCloset = arr[0, 1, 2];
+console.log(arr[i]);
+}
