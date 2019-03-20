f5f98f25cd8f2a46437665590b1ed04c86b035f5
Destiny <dshillis1@gmail.com>
2019-03-20 17:32:37 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -31,6 +31,59 @@ let catPhrase = ['...human...why you taking pictures of me?...', '...the catnip 
 for (i = 1; i <= 20; i++) {
     if (i % 2 === 0) {
         let random = catPhrase[Math.floor(Math.random()*catPhrase.length)];
-        console.log(random);
+//        console.log(random);
+    }
+}
+
+
+//Fizz Buzz
+//1. Write a javascript application that logs all numbers from 1 - 100.
+
+for (i = 1; i <= 100; i++) {
+//    console.log(i);
+}
+
+
+//2. If a number is divisible by 3 log "Fizz" instead of the number.
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+    console.log('Fizz');
+    }
+    else {
+        console.log(i);
+    }
+}
+
+
+//3. If a number is divisible by 5 log "Buzz" instead of the number.
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+ //   console.log('Fizz');
+    }
+    if (i % 5 === 0) {
+ //       console.log('Buzz');
+    }
+    else {
+ //       console.log(i);
+    }
+}
+
+
+//4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+
+for (i = 1; i <= 100; i++) {
+    if (i % 3 === 0) {
+//    console.log('Fizz');
+    }
+    if (i % 5 === 0) {
+//        console.log('Buzz');
+    }
+    if (i % 3 === 0 && i % 5 === 0) {
+//        console.log('FizzBuzz');
+    }
+    else {
+//        console.log(i);
     }
 }
