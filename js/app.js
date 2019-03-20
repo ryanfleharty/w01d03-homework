3db7fd35ee5e4995fa95c6bb8e265d4f251fb35c
Adam Wolfman <adamwolfman@Adams-MacBook-Pro.local>
2019-03-20 16:23:56 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -11,3 +11,21 @@ for( let i=1; i<=200; i++){
         console.log(i)
     }
 };
+
+// Excited Kitten: code that logs "Love me, pet me! HSSSSSS" 20 times
+
+for (let i=1; i<=20; i++){
+    if (i % 2 === 0){
+        let x = Math.floor(Math.random() * 3);
+        if (x === 0){
+            console.log("...human...why you taking pictures of me?...")
+        } else if (x === 1){
+            console.log("...the catnip made me do it")
+        } else {
+            console.log("...why does the red dot always get away...")
+        }
+    } else {
+        console.log("Love me, pet me! HSSSSSS")
+    }
+};
+
