cc4eaf66c60902e0569e7791c089737108e7a30d
Ben Wall <benwall@Bens-MacBook-Air.local>
2019-03-20 16:00:05 -0600

    get even answered

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
-for (let i = 1; i < 20; i++) {
-  console.log(i);
+
+// easy going
+
+// for (let i = 1; i < 20; i++) {
+//   console.log(i);
+// }
+
+// get even
+
+for (let i = 0; i < 200; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
