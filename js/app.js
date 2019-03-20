4b3f5a4f2674de63a532bb9eec30f165808583f8
WillyD <sopwill3@gmail.com>
2019-03-20 17:03:36 -0600

    getting to know you and yelling at ninja turtles finished

--- js/app.js
+++ js/app.js
@@ -33,4 +33,44 @@ for (let i = 1; i <= 100; i++) {
   i%3===0 ? console.log("Fizz") : i%5===0 ? console.log("Buzz") : i%3===0 && i%5===0 ? console.log("FizzBuzz") : console.log(i);
 }
 
-// GETTING TO KNOW YOU 1
+// GETTING TO KNOW YOU
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+const people = [kenny,jimHaff,reuben,jimClark,ryan];
+
+//1
+kenny[0] = "Gameboy";
+//2
+jimClark[1] += 1;
+//3
+ryan[2] = "Gotham";
+//4
+reuben.splice(2,1,"Chicago")
+//5
+jimHaff.splice(2,1,"Hong-Kong","Dubai","Tokyo")
+//6
+jimHaff.splice(2,1)
+
+console.log(people);
+
+// YELL AT THE NINJA TURTLES
+
+//1
+var reptiles = ["Donatello","Leonardo","Raphael","Michaelangelo"]
+
+//2
+for (i = 0; i < reptiles.length; i++) {
+  reptiles[i].toUpperCase;
+  console.log(reptiles[i]);
+}
+
+//3
+for (i = 0; i < reptiles.length; i++) {
+  console.log(reptiles[i].toUpperCase());
+  console.log(reptiles[i].toLowerCase());
+}
+
+// RETURN OF CLOSETS
