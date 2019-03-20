1bcce1bc280eff9773a292275acc169d63697f12
WillyD <sopwill3@gmail.com>
2019-03-20 16:37:07 -0600

    finished Kittens and FizzBuzz

--- js/app.js
+++ js/app.js
@@ -9,3 +9,28 @@ for (let i = 1; i <= 20; i++) {
 for (let i = 0; i <= 200; i+=2) {
   console.log(i);
 }
+
+// EXCITED KITTEN -- 1
+
+for (let i = 1; i <= 20; i++) {
+  console.log("Love me, pet me! HSSSSSS!");
+}
+
+// EXCITED KITTEN -- 2
+
+const meows = ["...human...why you taking pictures of me?...","...the catnip made me do it...","...why does the red dot always get away..."]
+
+for (let i = 1; i <= 20; i++) {
+  console.log("Love me, pet me! HSSSSSS!");
+  if (i % 2 === 0) {
+    var x = Math.floor(Math.random() * 3);
+    console.log(meows[x]);
+  }
+}
+
+// Fizz Buzz -- 1-4
+for (let i = 1; i <= 100; i++) {
+  i%3===0 ? console.log("Fizz") : i%5===0 ? console.log("Buzz") : i%3===0 && i%5===0 ? console.log("FizzBuzz") : console.log(i);
+}
+
+// GETTING TO KNOW YOU 1
