eb71effe59f8adccb72ec19f742021c7f78d46e0
Destiny <dshillis1@gmail.com>
2019-03-20 17:17:18 -0600

    Commit 3 - Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 //1. Write a for loop that will log the numbers 1 through 20.
 
 for (i = 1; i <= 20; i++) {
-    console.log(i);
+//    console.log(i);
 }
 
 //Get Even
@@ -12,6 +12,25 @@ for (i = 1; i <= 20; i++) {
 
 for (i = 0; i <= 200; i++) {
     if (i % 2 === 0) {
-        console.log(i);
+//        console.log(i);
+    }
+}
+
+//Excited Kitten
+//1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+
+for (i = 1; i <= 20; i++) {
+//    console.log('Love me, pet me! HSSSSSS!');
+}
+
+
+//2. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+
+let catPhrase = ['...human...why you taking pictures of me?...', '...the catnip made me do it...', '...why does the red dot always get away...'];
+
+for (i = 1; i <= 20; i++) {
+    if (i % 2 === 0) {
+        let random = catPhrase[Math.floor(Math.random()*catPhrase.length)];
+        console.log(random);
     }
 }
