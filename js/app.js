9f358396d7f5036c005b5fc26ffb1adbd7418dd3
John Oro <johnmichaeloro@gmail.com>
2019-03-20 16:19:47 -0600

    Commit 1 - Easy Going answered

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//Inside the directory created after you fork and clone, put your answers in app.js.
+//Keep your answers organized! Included a commented header for each section in your answers file.
+//If you're stuck on something for "too long" or get overly frustrated, make a comment and move on. Return to it later.
+//After each section (even if you're not fully done with it), add and commit your work. Use the commit messages that you see at the end of each section. If you go back and fix a section, commit again with an updated message.
+//When you are done with your homework, push to your fork and then and make a pull request.
+
+//Easy Going (Task 1)
+for(i = 1; i <= 20; i++) {
+  console.log(i);
+}
