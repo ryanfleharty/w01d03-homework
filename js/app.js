c24af0fbb7132b333a2854bfb97962d3ce661306
Collin Brockway <drbeep@Collins-MacBook-Pro.local>
2019-03-20 13:21:59 -0600

    Commit 1 - Easy Going answered

---
+++ js/app.js
@@ -0,0 +1,18 @@
+//|=========================================|
+//|											|
+//|			  Collin Brockway				|
+//|			SEI w01d03-homework				|
+//|			      3-20-2019					|
+//|											|
+//|=========================================|
+
+//Indicate successful loading:
+console.log("app.js loaded");
+
+//=======EASY GOING=======
+
+//for loop which logs the numbers 1 through 20
+for (let i = 1; i <= 20; i++)
+{
+	console.log(i);
+}
