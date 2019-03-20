e96b94face80bddfe9e5e1e16f416f2e0c38637e
Tori Snowden <tori@Toris-MacBook-Pro.local>
2019-03-20 15:56:03 -0600

    Commit 3 - comeback not done

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,14 @@
-//1.
+//1. Easy Going
 //for ( var i = 1; i < 20; i++) {
   //  console.log(i)
 //}
 
-//2. 
-for (var i = 0; i < 200; i+=2)
-console.log(i)
+//1. Get Even
+//for (var i = 0; i < 200; i+=2)
+//console.log(i)
+
+//1. Excited Kitten 
+for (var i = 0; i < 20; i++)
+console.log('Love me, pet me! HSSSSSS')
+
 
