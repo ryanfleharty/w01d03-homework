2cef5cdf7ce524e53be0c7569cec109010115e98
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-03-20 17:43:04 -0600

    Commit 4.1 - Fizz Buzz answered but right this time.

--- app.js
+++ app.js
@@ -26,15 +26,17 @@ console.log(petThoughts1);
 
 //Fizz Buzz
 for (let i = 1; i <= 100; i++) {
-	if (i % 3 === 0) {
+	if ((i % 3 === 0) && (i % 5 === 0)) {
+		console.log('FizzBuzz');
+	} else if (i % 3 === 0) {
 		console.log('Fizz');
 	} else if (i % 5 === 0) {
 		console.log('Buzz');
-	} else if ((i % 3 === 0) && (i % 5 === 0)) {
-		console.log('FizzBuzz');
-	}
+	} else {
 	console.log(i);
 }
+}
+
 
 
 
