56385adc495d9725b6e9c0e6e17d5c9d7d062d55
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-20 17:24:19 -0600

    Commit 3 - Excited Kittins answered

--- homework/js/app.js
+++ homework/js/app.js
@@ -6,6 +6,25 @@ for(let i = 0; i <= 20; i++){
 }
 
 //Get Even
-for(let i = 0; i<=200; i+=2){
+for(let i = 0; i <= 200; i+=2){
 	console.log(i);
 }
+
+//Excited Kitten
+//1. 
+for(let i = 0; i < 20; i++){
+	console.log("Love me, pet me! HSSSSSS");
+}
+//2.
+let catWords = ["...human...why you taking pictures of me?...", 
+"...the catnip made me do it...", 
+"...why does the red dot always get away..."];
+let arrayLength = catWords.length;
+
+for(let i = 0; i < 20; i++){
+	if(i % 2 === 0){
+		let random = Math.floor(Math.random() * arrayLength);
+		console.log(catWords[random]);
+	}
+}
+
