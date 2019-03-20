1755b3ea9f9315d3ed52a3ebcc480103719e6768
Matthew Puettmann <mattpuettmann@Matthews-MacBook-Pro.local>
2019-03-20 17:34:50 -0600

    Commit 4 - FizzBuzz answered

--- app.js
+++ app.js
@@ -22,8 +22,20 @@ for (let i = 0; i < 20; i++) {
 	console.log(rand);
 	} 
 console.log(petThoughts1);
-	
+	}
+
+//Fizz Buzz
+for (let i = 1; i <= 100; i++) {
+	if (i % 3 === 0) {
+		console.log('Fizz');
+	} else if (i % 5 === 0) {
+		console.log('Buzz');
+	} else if ((i % 3 === 0) && (i % 5 === 0)) {
+		console.log('FizzBuzz');
+	}
+	console.log(i);
 }
+
 
 
 
