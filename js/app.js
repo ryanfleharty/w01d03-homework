3b35b6c347369c7cfc394dfcd14ba26b0fe03832
Jake Shelton <jakeshelton@Archies-Howl.local>
2019-03-20 17:21:19 -0600

    Commit 6 - Yell at the Ninja Turtles answered

--- js/app.js
+++ js/app.js
@@ -1,16 +1,16 @@
 console.log('Up and running');
 
-// Easy Going
-// 1. Write a for loop that will log the numbers 1 through 20.
-// for (let i = 1; i <= 20; i += 1) {
+// // Easy Going
+// // 1. Write a for loop that will log the numbers 1 through 20.
+// // for (let i = 1; i <= 20; i += 1) {
+// //   console.log(i);
+// // }
+
+// // Get Even
+// // 1. Write a for loop that will log only the even numbers in 0 through 200.
+// for (let i = 0; i <= 200; i += 2) {
 //   console.log(i);
 // }
-
-// Get Even
-// 1. Write a for loop that will log only the even numbers in 0 through 200.
-for (let i = 0; i <= 200; i += 2) {
-  console.log(i);
-}
 
 // // Excited Kitten
 // // 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
@@ -48,25 +48,25 @@ for (let i = 0; i <= 200; i += 2) {
 //   }
 // }
 
-// // Fizz Buzz
-// // NOTE: THIS IS A CLASSIC RITE-OF-PASSAGE FOR JAVASCRIPT PROGRAMMERS, ESPECIALLY ONES AT GA
-// // 1. Write a javascript application that logs all numbers from 1 - 100.
-// // 2. If a number is divisible by 3 log "Fizz" instead of the number.
-// // 3. If a number is divisible by 5 log "Buzz" instead of the number.
-// // 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
-// const max = 100;
-// for (let i = 0; i < max; i += 1) {
-//   if ((i % 3 === 0) && (i % 5 === 0)) {
-//     result = 'fizzbuzz';
-//   } else if (i % 3 === 0) {
-//     result = 'fizz';
-//   } else if (i % 5 === 0) {
-//     result = 'buzz';
-//   } else {
-//     result = i;
-//   }
-//   console.log(result);
-// }
+// Fizz Buzz
+// NOTE: THIS IS A CLASSIC RITE-OF-PASSAGE FOR JAVASCRIPT PROGRAMMERS, ESPECIALLY ONES AT GA
+// 1. Write a javascript application that logs all numbers from 1 - 100.
+// 2. If a number is divisible by 3 log "Fizz" instead of the number.
+// 3. If a number is divisible by 5 log "Buzz" instead of the number.
+// 4. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+const max = 100;
+for (let i = 0; i < max; i += 1) {
+  if ((i % 3 === 0) && (i % 5 === 0)) {
+    result = 'fizzbuzz';
+  } else if (i % 3 === 0) {
+    result = 'fizz';
+  } else if (i % 5 === 0) {
+    result = 'buzz';
+  } else {
+    result = i;
+  }
+  console.log(result);
+}
 
 // Getting to Know You
 // Use the following arrays to answer the questions below (name, age, hometown):
@@ -100,5 +100,30 @@ console.table(jim);
 // 6. Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
 jim.splice(2,1);
 console.table(jim);
+
+// Yell at the Ninja Turtles
+// 1. Create an array with the members of the ninja turtles (Donatello, Leonardo, Raphael, Michaelangelo)
+const turtlesArr = ['Donatello', 'Leonardo', 'Raphael', 'Michaelangelo'];
+
+// 2. Use a for loop to call .toUpperCase() on each of them and print out the result.
+for (let i = 0; i < turtlesArr.length; i++) {
+  console.log(turtlesArr[i].toUpperCase());
+}
+
+// 3. Bonus: Modify the answer you just wrote. Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
+for (let i = 0; i < turtlesArr.length; i++) {
+  const turtleNameArr = turtlesArr[i].split('');
+  turtlesArr[i] = '';
+  for (let x = 0; x < turtleNameArr.length; x++) {
+    if (x % 2 === 0) {
+      turtlesArr[i]+=turtleNameArr[x].toUpperCase(); 
+    } else {
+      turtlesArr[i]+=turtleNameArr[x].toLowerCase(); 
+    }
+  }
+  console.log(turtlesArr[i]);
+}
+
+
 
 
