4eba2175ddc62dae1c6f24b298d96001f1dbe7b5
Alexandra Thornton <alexandrathornton@Alexandras-MacBook-Pro.local>
2019-03-20 17:41:18 -0600

    Commit 4 - Fizz Buzz answered

--- homework/js/app.js
+++ homework/js/app.js
@@ -28,3 +28,16 @@ for(let i = 0; i < 20; i++){
 	}
 }
 
+//Fizz Buzz
+for(let i = 1; i <=100; i++){
+	if ((i % 3 === 0) && (i % 5 === 0)) {
+		console.log("Fizz Buzz");
+	} else if(i % 3 === 0){
+		console.log("Fizz");
+	} else if(i % 5 === 0){
+		console.log("Buzz");
+	} else {
+		console.log(i);
+	}
+}
+
