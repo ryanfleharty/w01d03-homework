722519a0970239b3d157a1f57ef236420c2e1fda
keenmaster486 <cebrock106@gmail.com>
2019-03-20 14:35:27 -0600

    Commit 5 - Getting to Know You answered

--- js/app.js
+++ js/app.js
@@ -59,3 +59,18 @@ for (let i = 1; i <= 100; i++)
 	console.log( ((!(!(i % 3) || !(i % 5)) && i) || "") + ((!(i % 3) && "Fizz") || "") + ((!(i % 5) && "Buzz") || "") );
 }
 
+
+//=======GETTING TO KNOW YOU=======
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+shahzad[0] = "Gameboy";
+daniel[1]++;
+ryan[2] = "Gotham City";
+reuben.pop(); reuben.push("Chicago");
+jim.pop(); jim.push("Boise", "Boston", "London");
+jim.splice(2, 1);
