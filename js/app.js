cad730e279823fa01543ea661d90c8eb4e6ae83d
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-03-20 16:28:36 -0600

    Commit 3- Excited Kittens answered

--- js/app.js
+++ js/app.js
@@ -10,4 +10,9 @@ for(i=0; i<=200; i+=2){
 	console.log('even number is: ' + i);
 }
 
-//
+// EXCITED KITTEN 
+ for(let i = 1; i<=20; i++){
+ 	console.log("Love me, pet me! HSSSSS: " +i);
+ for(let i = 1; i<=20; i+=2);
+ 	console.log("..the catnip made me do it..: " +i)
+ }
