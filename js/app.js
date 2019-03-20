4bf0cce8e227ac1f2f6d91a1c68b4bed64568710
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-20 07:59:12 -0600

    Commit 2 - Get Even answered

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 //Write a for loop that will log the numbers 1 through 20.
-for (let i = 1;i<=20;i++){
+/*for (let i = 1;i<=20;i++){
     console.log(i)
+}
+*/
+for (let i=0;i<=200;i+=2){
+    console.log(i);
 }
