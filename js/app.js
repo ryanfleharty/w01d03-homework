4dcab1b1afbfad83d44db25221dce478a1ee56b1
Destiny <dshillis1@gmail.com>
2019-03-20 21:54:53 -0600

    Commit 5 - Getting to Know You answered - well, most of it...

--- js/app.js
+++ js/app.js
@@ -48,10 +48,10 @@ for (i = 1; i <= 100; i++) {
 
 for (i = 1; i <= 100; i++) {
     if (i % 3 === 0) {
-    console.log('Fizz');
+ //   console.log('Fizz');
     }
     else {
-        console.log(i);
+  //      console.log(i);
     }
 }
 
@@ -87,3 +87,63 @@ for (i = 1; i <= 100; i++) {
 //        console.log(i);
     }
 }
+
+
+//Getting to Know You
+//Use the following arrays to answer the questions below (name, age, hometown):
+
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+//1. Daniel decides that Shahzad can't be named "Shahzad" anymore. Remove "Shahzad" from the shahzad array and replace it with "Gameboy".
+
+shahzad[0] = 'Gameboy';
+console.log(shahzad);
+
+//2. Daniel just had his birthday; change Daniel's array to reflect him being a year older. Don't just hard code 187--pretend that you don't know that his age is 186, and write your code to just make him a year older than whatever age he was.
+//NOT WORKING...NEED TO FIX
+
+let age = daniel.splice(1, 1);
+//console.log(age);
+
+let newAge = age + 1;
+//console.log(newAge);
+
+
+//3. Ryan is Batman maybe. Or possibly Robin. Change Ryan's hometown from "Denver" to "Gotham City".
+
+ryan[2] = 'Gotham City';
+//console.log(ryan);
+
+//4. Reuben left Durham 5 years ago to come to Chicago. Remove "Durham" from Reuben's array and add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+
+reuben.pop();
+//console.log(reuben);
+
+newTown = 'Chicago';
+reuben.push(newTown);
+//console.log(reuben);
+
+//or a better way would be...
+
+reuben.splice(2, 1, 'Chicago')
+//console.log(reuben);
+
+
+//5. Jim could be literally anywhere in the world. Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+
+jim.splice(2, 1, 'San Francisco', 'Santo Domingo', 'Santiago');
+//console.log(jim);
+
+
+//6. Bonus: Whoops! Jim is actually only allowed to be in one of two cities. Whatever the first of the 3 cities for Jim is now, remove it from the array using .splice()
+//DO THIS????
+
+
+
+
+
+
