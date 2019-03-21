5092cb75212749a6e0fcc38d5ebabe150f727d84
Adam Wolfman <adamwolfman@Adams-MBP.domain>
2019-03-20 18:40:49 -0600

    Commit 9 - Project Euler Problem 1 answered

--- js/app.js
+++ js/app.js
@@ -170,6 +170,19 @@ for (i = 0; i < thomsCloset.length; i++){
 };
 
 
+//MULTIPLES OF 3 OR 5 *PROJECT EULER*
+//find the sum of all of the multiples of 3 or 5 below 1000
+
+let sum = 0;
+
+for (let i = 0; i <= 1000; i++){
+    if(i % 3 === 0 || i % 5 === 0){
+        sum = sum + i;
+    }   
+};
+
+console.log(sum);
+
 
 
 
