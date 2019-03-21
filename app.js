f48ffbbaeec0a42409ade5720e74a349fbdd7f74
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-03-20 21:24:39 -0600

    Fizz Buzz answered

--- app.js
+++ app.js
@@ -13,14 +13,39 @@
 // for(let i = 0; i = 20; i++)
 // 	console.log(Love me, pet me! HSSSSSS!);
 
-2//
-petThoughts1["Love me, pet me! HSSSSSS!"];
-petThoughts2["...human...why you taking pictures of me?...", "...the catnip made me do it...",  "...why does the red dot always get away..."];
+//2
+// petThoughts1["Love me, pet me! HSSSSSS!"];
+// petThoughts2["...human...why you taking pictures of me?...", "...the catnip made me do it...",  "...why does the red dot always get away..."];
 
-for (let i = 0; i < 20; i++) {
-    if (i % 2 === 0) {
-        const rand = petThoughts2[Math.floor(Math.random() * petThoughts2.length)];
-    console.log(rand);
-    } 
-console.log(petThoughts1);
-    }
+// for (let i = 0; i < 20; i++) {
+//     if (i % 2 === 0) {
+//         const rand = petThoughts2[Math.floor(Math.random() * petThoughts2.length)];
+//     console.log(rand);
+//     } 
+// console.log(petThoughts1);
+//     }
+
+// Fizz Buzz
+
+//1.for(let i = 0; i <= 100; i++)
+	console.log(i);
+//2
+for (let i = 1; i <= 100; i++){
+  if (i % 3 == 0) console.log("Fizz");
+  else console.log(i);
+}
+
+//3
+for (let i = 1; i <= 100; i++){
+  if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("buzz");
+  else console.log(i);
+}
+//4
+for (var i=1; i < 101; i++){
+  if (i % 15 == 0) console.log("FizzBuzz");
+  else if (i % 3 == 0) console.log("Fizz");
+  else if (i % 5 == 0) console.log("Buzz");
+  else console.log(i);
+}
+
