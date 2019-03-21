7981f45addad0d7b7260c16805e35907a838b7c9
David Krieger <davidkrieger@Davids-MacBook-Air.local>
2019-03-20 22:01:56 -0600

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ For every even number in your loop, log
 "...human...why you taking pictures of me?...", 
 "...the catnip made me do it...", or 
 "...why does the red dot always get away..." at random.
-*/
+
     for (let i=1;i<=20;i++){
         let speakMeow = Math.floor(Math.random(i)*(4-1)+1)
         if (i%2===0 && speakMeow ===1){
@@ -28,3 +28,14 @@ For every even number in your loop, log
             console.log("...why does the red dot always get away...")
         }
      else {console.log("Love me, pet me! HSSSSSS!")}}
+*/
+//FIZZBUZZ
+//Write a javascript application that logs all numbers from 1 - 100.
+for(let i = 1;i<=100;i++)
+//If a number is divisible by 3 log "Fizz" instead of the number.
+{if (i % 5 === 0 && i % 3 === 0){console.log("FizzBuzz")}
+//If a number is divisible by 5 log "Buzz" instead of the number.
+else if (i % 5 === 0){console.log("Buzz")}
+//If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number.
+else if (i % 3 === 0){console.log("Fizz")}
+else {console.log(i)}}
