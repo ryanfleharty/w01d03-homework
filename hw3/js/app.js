5f9d99d092b6669dc9f1eb9244f68cbe86782e47
Davis Truong <buckets_truong@Daviss-MacBook-Pro.local>
2019-03-20 21:14:41 -0600

    excited kittens answered

--- hw3/js/app.js
+++ hw3/js/app.js
@@ -1,8 +1,25 @@
 //easy going
-/*for(let i = 1; i < 20; i++){
+for(let i = 1; i < 20; i++){
 console.log(+i);
-}*/
+}
 //get even
 for(let i = 0; i <= 200; i += 2){
   console.log(i);
+}
+//excited kitten
+for(let i = 1; i <=20; i++){
+  console.log('love me, pet me! HSSSSSS!');
+  if (i % 2 == 0){
+    let t1 = "...human...why you taking pictures of me?..."
+    let t2 = "...the catnip made me do it..."
+    let t3 = "...why does the red dot always get away..."
+      let rand = Math.random()
+        if (rand < .33){
+          console.log(t1)
+        } else if (rand > .33 && rand < .66) {
+            console.log(t2);
+        } else {
+            console.log(t3);
+        }
+  }
 }
