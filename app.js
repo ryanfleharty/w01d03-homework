54de395c356998ffd0d847ca723d6a9dfe5cf7be
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-03-21 09:01:30 -0600

    Commit 9- Project Euler Problem 1 answered

--- app.js
+++ app.js
@@ -98,14 +98,15 @@ kristynsCloset.splice(0,1, "Chicago");
 console.log(reuben);
 
 
+
+//
+// Dirty Laundry
+
+
 //Multiples of 3 and 5
 const ans [];
 const j = 0;
 for (let i = 1; i < 1000; i++) {
     if (i % 3 === 0 || i % 5 === 0) {
-        ans[j] = i;
-        sum += i;
-    }
+  console.log(sum +=i);
 }
-
-// Dirty Laundry
