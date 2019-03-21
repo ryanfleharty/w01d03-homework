153611c96f03aa4a1b9615cdc4e9503caf7c6b18
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-03-20 18:04:57 -0600

    Commit 4- FizzBuzz answered

--- js/app.js
+++ js/app.js
@@ -16,3 +16,30 @@ for(i=0; i<=200; i+=2){
  for(let i = 1; i<=20; i+=2);
  	console.log("..the catnip made me do it..: " +i)
  }
+
+ // FIZZ BUZZ
+for(let i =1; i<=100; i++){
+	if(i % 3 )
+			console.log("fizz");
+	else if (i % 5)
+			console.log("Buzz");
+	else if (i % 15)
+			console.log("FizzBuzz");
+		
+		console.log(i);
+	}
+// GETTING TO KNOW YOU
+
+// 1.) 
+const shahzad = ["Shahzad", 1000, "Austin"];
+const jim = ["Jim", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const daniel = ["Daniel", 186, "Dallas"];
+const ryan = ["Ryan", 65, "Denver"];
+
+
+	shahzad.splice(0, "Gameboy");
+	daniel.splice(0, 1, 187);
+// WILL COME BACK TO
+
+
