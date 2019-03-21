d6ab6721321cd163f43aa36c4866d3d764fa47be
Joe Luchini <joeluchini@Joes-MacBook-Pro.local>
2019-03-21 07:02:46 -0600

    yell at ninja turtles answered

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-// //Easy Going
-// for(let i = 0; i <= 20; i++)
-//   console.log(i);
+//Easy Going
+for(let i = 0; i <= 20; i++)
+  console.log(i);
 
-// //Get Even
-// for(let i = 0; i <= 200; i++)
-// 	if (i % 2 === 0) {
-// 		console.log(i);
-// 	}
+//Get Even
+for(let i = 0; i <= 200; i++)
+	if (i % 2 === 0) {
+		console.log(i);
+	}
 
-// //Excited Kitten
-// 1//
-// for(let i = 0; i = 20; i++)
-// 	console.log(Love me, pet me! HSSSSSS!);
+//Excited Kitten
+1//
+for(let i = 0; i = 20; i++)
+	console.log(Love me, pet me! HSSSSSS!);
 
-//2
-// petThoughts1["Love me, pet me! HSSSSSS!"];
-// petThoughts2["...human...why you taking pictures of me?...", "...the catnip made me do it...",  "...why does the red dot always get away..."];
+2
+petThoughts1["Love me, pet me! HSSSSSS!"];
+petThoughts2["...human...why you taking pictures of me?...", "...the catnip made me do it...",  "...why does the red dot always get away..."];
 
-// for (let i = 0; i < 20; i++) {
-//     if (i % 2 === 0) {
-//         const rand = petThoughts2[Math.floor(Math.random() * petThoughts2.length)];
-//     console.log(rand);
-//     } 
-// console.log(petThoughts1);
-//     }
+for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+        const rand = petThoughts2[Math.floor(Math.random() * petThoughts2.length)];
+    console.log(rand);
+    } 
+console.log(petThoughts1);
+    }
 
 // Fizz Buzz
 
@@ -78,5 +78,20 @@ console.log(reuben);
 
 //5 /// could not verify if correct
 jim.splice (2,3, "Portland", "Boston", "Sacramento");
+
+//Yell at the Ninja Turtles
+
+//1.
+const ninjaTurles = ["Donatello", "Leonardo",
+ "Rapael", "Michaelangelo"];
+
+//2. SKIP
+// couldnt figure out how to use for loop
+// played around with .map function, but
+//also couldnt get that one to work. 
+toUpper
+
+
+
 
 
