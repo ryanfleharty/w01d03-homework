d16cd6a3ee2579887c4d259ba2e744f116b4b8e1
Veda Anderson <vedaanderson@Vedas-MacBook-Pro.local>
2019-03-20 21:32:16 -0600

    Commit 9 - Project Euler Problem 1 answered/ commit 8 to be fixed

--- js/app.js
+++ js/app.js
@@ -19,15 +19,16 @@ for(i=0; i<=200; i+=2){
 
  // FIZZ BUZZ
 for(let i =1; i<=100; i++){
-	if(i % 3 )
+	if(i % 3 == 0)
 			console.log("fizz");
-	else if (i % 5)
+	else if (i % 5 == 0)
 			console.log("Buzz");
-	else if (i % 15)
+	else if (i % 15 == 0)
 			console.log("FizzBuzz");
-		
-		console.log(i);
+		else
+		console.log(i);	
 	}
+	
 // GETTING TO KNOW YOU
 
 // 1.) 
@@ -40,6 +41,85 @@ const ryan = ["Ryan", 65, "Denver"];
 
 	shahzad.splice(0, "Gameboy");
 	daniel.splice(0, 1, 187);
-// WILL COME BACK TO
+
+
+
+
+// YELL AT THE NINJA TURTLES
+
+var ninjaTurtles = ["Donatello", "leonardo", "Rapheal", "Michealangelo"];
+
+for(let i=0; i < ninjaTurtles.length; i++){
+
+	console.log(ninjaTurtles[i].toUpperCase());
+	console.log(i);
+}
+	
+// RETURN OF CLOSETS
+
+const kristynsCloset = [
+  "left shoe",
+  "cowboy boots",
+  "right sock",
+  "GA hoodie",
+  "green pants",
+  "yellow knit hat",
+  "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+  [
+    // These are Thom's shirts
+    "grey button-up",
+    "dark grey button-up",
+    "light blue button-up",
+    "blue button-up",
+  ],[
+    // These are Thom's pants
+    "grey jeans",
+    "jeans",
+    "PJs"
+  ],[
+    // Thom's accessories
+    "wool mittens",
+    "wool scarf",
+    "raybans"
+  ]
+];
+
+var move = kristynsCloset[0];
+
+var endOf = thomsCloset.length 
+// borken will come back to fix
+
+
+
+// MULTIPLES OF 3 & 5
+	for(let i=0; i<=1000; i++){
+		if (i % 3 == 0)
+			console.log('show number : ' +i);
+		else if (i % 5 == 0)
+			console.log('show number : ' +i);
+		else  
+			console.log(i);
+	}
+
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
